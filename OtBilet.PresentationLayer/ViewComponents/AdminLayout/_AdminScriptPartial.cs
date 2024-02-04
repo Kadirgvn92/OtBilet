6fdafcea1332d08f838026eb1a43a78ca1481d0f
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-02-04 16:30:22 +0300

    UI design refactorred

---
+++ OtBilet.PresentationLayer/ViewComponents/AdminLayout/_AdminScriptPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OtBilet.PresentationLayer.ViewComponents.AdminLayout;
+
+public class _AdminScriptPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();  
+    }
+}
