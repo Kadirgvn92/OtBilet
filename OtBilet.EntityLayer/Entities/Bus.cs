0244ae9a5e25c6fcff66529f640b7a0981923478
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-02-06 14:29:32 +0300

    Ticket page desinged

--- OtBilet.EntityLayer/Entities/Bus.cs
+++ OtBilet.EntityLayer/Entities/Bus.cs
@@ -12,6 +12,8 @@ public class Bus
     public int BusID { get; set; }
     public string Plate { get; set; }
     public int PassengerCount { get; set; }
+    public string FirmName { get; set; }
+    public string FirmImageUrl { get; set; }
     public bool Wifi { get; set; }
     public BusType BusType { get; set; }
     public List<Destination> Destinations { get; set; }
